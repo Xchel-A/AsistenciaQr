2bbe36fa2aa97d2de4d4f95b5c9117429e045d4b
Alejandro <2020371028@uteq.edu.mx>
2023-11-30 20:15:55 -0600

    Logos, Diseño e iconos

--- src/screens/DataUser.js
+++ src/screens/DataUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 import { useAuth } from '../providers/AuthProvider';
+
 
 const DataUser = () => {
   const { user, signOut } = useAuth();
@@ -11,33 +12,71 @@ const DataUser = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Correo:</Text>
-        <Text style={styles.value}>{user.email}</Text>
+
+    <SafeAreaView style={styles.container}>
+
+      <Image style={styles.imagen} source={require('../../assets/uteq.png')} />
+
+      <View style={styles.conten}>
+        <Text style={styles.saludo}>¡Hola, {user.displayName}!</Text>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Correo:</Text>
+          <Text style={styles.value}>{user.email}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Tipo de usuario:</Text>
+          <Text style={styles.value}>{user.role}</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.boton}
+          onPress={handleSignOut}
+        >
+          <Text style={styles.textoBoton}>Cerrar Sesión</Text>
+        </TouchableOpacity>
+
       </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Nombre:</Text>
-        <Text style={styles.value}>{user.displayName}</Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Tipo de usuario:</Text>
-        <Text style={styles.value}>{user.role}</Text>
-      </View>
-      <Button title="Cerrar Sesión" onPress={handleSignOut} />
-    </View>
+
+    </SafeAreaView>
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    height: '100%',
     alignItems: 'center',
   },
+  imagen: {
+    height: 110,
+    width: 260,
+    margin: 40,
+    marginBottom: 70
+  },
+  conten: {
+    borderRadius: 15,
+    height: 330,
+    width: 270,
+    backgroundColor: "#ABCCE7",
+
+  },
+
   infoContainer: {
     flexDirection: 'row',
-    marginBottom: 10,
+    marginBottom: 35,
+    width: 180,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  saludo: {
+    marginBottom: 50,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 35,
+    fontWeight: 'bold',
+    fontSize: 16
   },
   label: {
     fontWeight: 'bold',
@@ -45,6 +84,20 @@ const styles = StyleSheet.create({
   },
   value: {
     fontSize: 16,
+  },
+  boton: {
+    marginTop: 20,
+    width: 150,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: '#004E8B',
+    borderRadius: 15,
+    padding: 10,
+  },
+  textoBoton: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
 
